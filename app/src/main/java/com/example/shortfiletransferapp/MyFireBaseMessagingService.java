5375be403a69665a06a40f519dd757aa8c289161
HyunHoMoon <44826442+gusgh9176@users.noreply.github.com>
2020-06-01 17:29:28 +0900

    서버에 FCM Token 전송 구현

--- app/src/main/java/com/example/shortfiletransferapp/MyFireBaseMessagingService.java
+++ app/src/main/java/com/example/shortfiletransferapp/MyFireBaseMessagingService.java
@@ -9,40 +9,63 @@ import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.util.Log;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
-import okhttp3.FormBody;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 
 
 public class MyFireBaseMessagingService extends FirebaseMessagingService {
+
+    // 처음 앱 설치시에만 해당 함수 실행 됨
     @Override
-    public void onNewToken(String token) {
+    public void onNewToken(@NonNull String token) {
         super.onNewToken(token);
         Log.d("FCM Log", "Refreshed token: " + token);
 
         // https://yamea-guide.tistory.com/227
         // 생성한 토큰을 서버로 날려서 저장하기 위해서 만든거
-//        sendRegistrationToServer(token);
+        sendRegistrationToServer(token);
     }
+
+
+    // 메시지를 받았을 때 실행되는 함수
     @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
-        if (remoteMessage.getNotification() != null) {
-            Log.d("FCM Log", "알림 메시지: " + remoteMessage.getNotification().getBody());
-            String messageBody = remoteMessage.getNotification().getBody();
-            String messageTitle = remoteMessage.getNotification().getTitle();
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        // 알람 Title, 알람 Body
+        try {
+            sendNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+        }catch (NullPointerException e){
+            e.printStackTrace();
+        }
+    }
+
+    // 메시지를 받았을 때 처리하는 함수 (알람 보여줌)
+    private void sendNotification(String messageTitle, String messageBody) {
+
+        if (messageTitle != null && messageBody != null) {
+            Log.d("FCM Log", "알림 메시지: " + messageBody);
+
             Intent intent = new Intent(this, MainActivity.class);
+
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
             String channelId = "Channel ID";
             Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
             NotificationCompat.Builder notificationBuilder =
                     new NotificationCompat.Builder(this, channelId)
                             .setSmallIcon(R.mipmap.ic_launcher)
@@ -51,27 +74,42 @@ public class MyFireBaseMessagingService extends FirebaseMessagingService {
                             .setAutoCancel(true)
                             .setSound(defaultSoundUri)
                             .setContentIntent(pendingIntent);
+
             NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                String channelName = "Channel Name";
-                NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
-                notificationManager.createNotificationChannel(channel);
+
+            try {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    String channelName = "Channel Name";
+                    NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
+                    notificationManager.createNotificationChannel(channel);
+                }
+                notificationManager.notify(0, notificationBuilder.build());
+            } catch (NullPointerException e) {
+                e.printStackTrace();
             }
-            notificationManager.notify(0, notificationBuilder.build());
         }
     }
+
+    // 서버로 토큰 값 전송 (서버에서 해당 기기 토큰 값 저장하는 용도)
     private void sendRegistrationToServer(String token) {
-        // Add custom implementation, as needed.
+
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        JSONObject jsonInput = new JSONObject();
+        try {
+            jsonInput.put("name", "name"); // User name (변동 가능)
+            jsonInput.put("token", token); // User token (설치시 고정)
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
         // OKHTTP를 이용해 웹서버로 토큰값을 날려준다.
         OkHttpClient client = new OkHttpClient();
-        RequestBody body = new FormBody.Builder()
-                .add("Token", token)
-                .build();
+        RequestBody body = RequestBody.create(JSON, jsonInput.toString()); // JSON 담은 RequestBody 생성
 
         //request
         Request request = new Request.Builder()
-                .url("http://172.30.1.60:8081/mobile/sendFCM") // 토큰 저장하려고 보내는 URL / 추후 따로 토큰 저장하는 url 만들어야함 현재는 그냥 전체 푸시 알람 보내주는 URL 넣어뒀음
+//                .url("http://172.30.1.60:8080/mobile/insert/FCMToken") // 토큰 저장하려고 보내는 URL
+                .url("https://junior-programmer.com/mobile/insert/FCMToken")
                 .post(body)
                 .build();
 
