8b7aa3274325662d70139ee7bbed0ec9bb5853c9
HyunHoMoon <44826442+gusgh9176@users.noreply.github.com>
2020-06-15 00:44:30 +0900

    추가 유저VO

---
+++ app/src/main/java/com/example/shortfiletransferapp/vo/UserVO.java
@@ -0,0 +1,15 @@
+package com.example.shortfiletransferapp.vo;
+
+public class UserVO {
+
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
