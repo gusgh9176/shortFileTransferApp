d67fc06eec2232ce1bcbf51915e275434f17b8a0
HyunHoMoon <44826442+gusgh9176@users.noreply.github.com>
2020-06-21 20:00:41 +0900

    제거 기본 알림 코드
    FCM 제외한 불필요 알림 코드 삭제

--- app/src/main/java/com/example/shortfiletransferapp/MainActivity.java
+++ app/src/main/java/com/example/shortfiletransferapp/MainActivity.java
@@ -4,16 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,7 +20,6 @@ import android.widget.Toast;
 
 import com.example.shortfiletransferapp.adapter.ListViewAdapter;
 import com.example.shortfiletransferapp.permission.TedPermission;
-import com.example.shortfiletransferapp.utils.network.FileDownloadUtils;
 import com.example.shortfiletransferapp.utils.network.FileUploadUtils;
 import com.example.shortfiletransferapp.utils.GetFileNameUtils;
 import com.example.shortfiletransferapp.utils.network.GetUserListUtils;
@@ -88,32 +83,18 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xFF339999));
         // 액션바 설정 끝 //
 
-        // 푸쉬알람 설정하기 //
-        //푸시 알림을 보내기위해 시스템에 권한을 요청하여 생성
-        final NotificationManager notificationManager =
-                (NotificationManager) MainActivity.this.getSystemService(MainActivity.this.NOTIFICATION_SERVICE);
-        //푸시 알림 터치시 실행할 작업 설정(여기선 MainActivity로 이동하도록 설정)
-        final Intent intent = new Intent(MainActivity.this.getApplicationContext(), MainActivity.class);
-        //Notification 객체 생성
-        final Notification.Builder builder = new Notification.Builder(getApplicationContext());
-        //푸시 알림을 터치하여 실행할 작업에 대한 Flag 설정 (현재 액티비티를 최상단으로 올린다 | 최상단 액티비티를 제외하고 모든 액티비티를 제거한다)
-        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        // 푸쉬알람 설정 끝 //
-
-//        PushAlarm(notificationManager, intent, builder); // 푸쉬 알람 보냄
-
         Log.d("End: onCreate ", "End: onCreate ");
-        try {
-            Thread.sleep(3000); // 3초 정지 FCM 전송을 완료하고 목록 갱신 요청하기 위해 사용
-        }catch (InterruptedException ie){
-            ie.printStackTrace();
-        }
     }
 
     @Override
     protected void onStart(){
         super.onStart();
         Log.d("Start: onStart ", "Start: onStart ");
+        try {
+            Thread.sleep(3000); // 3초 정지 FCM 전송을 완료하고 목록 갱신 요청하기 위해 사용
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
 
         // FCM 실행시 저장해둔 token 값 가져옴
         // 서버에 요청하여 유저 목록 갱신
@@ -135,7 +116,6 @@ public class MainActivity extends AppCompatActivity {
                             return;
                         }
                         addItemAdapter(userVOS); // 유저 목록 갱신
-                        // addItemAdapter() 메소드 내부에서 생성한 listview에 클릭 이벤트 핸들러 정의.
                         // 유저 클릭시 파일 선택 창 열음
                         // 전송 관련 내용 https://derveljunit.tistory.com/302?category=523828
                         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -239,19 +219,7 @@ public class MainActivity extends AppCompatActivity {
             case R.id.action_refresh: // actionbar의 refresh 키 눌렀을 때 동작
 //                addItemAdapter();  // 메소드 통해 adapter와 listview를 재 생성 시켜줌
 
-                Toast.makeText(this, "실행", Toast.LENGTH_SHORT).show();
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        // http://egloos.zum.com/pavecho/v/7204359
-                        // 선택한 파일 임시 저장
-                        String date = new SimpleDateFormat("yyyy_MM_dd_hh_mm_ss").format(new Date());
-                        String savePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath() + "/" + date + ".";
-                        FileDownloadUtils.send2Server("testToken", savePath); // 다운로드 테스트
-                    }
-                }).start();
-
-//                Toast.makeText(this, "새로 고침", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "새로 고침", Toast.LENGTH_SHORT).show();
                 break;
         }
         return super.onOptionsItemSelected(item);
@@ -309,44 +277,5 @@ public class MainActivity extends AppCompatActivity {
         alertDialog.show();
     }
 
-    public void RequestSendingAlertDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-        builder.setTitle("전송 요청").setMessage("선택하세요.");
-
-        builder.setPositiveButton("수락", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
-                Toast.makeText(getApplicationContext(), "수락하였습니다.", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        builder.setNegativeButton("거절", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
-                Toast.makeText(getApplicationContext(), "거절하였습니다.", Toast.LENGTH_SHORT).show();
-            }
-        });
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
-    }
-
-    public void PushAlarm(NotificationManager notificationManager, Intent intent, Notification.Builder builder) {
-        //앞서 생성한 작업 내용을 Notification 객체에 담기 위한 PendingIntent 객체 생성
-        PendingIntent pendnoti = PendingIntent.getActivity(MainActivity.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        //푸시 알림에 대한 각종 설정
-        builder.setSmallIcon(R.drawable.ic_launcher_background).setTicker("Ticker").setWhen(System.currentTimeMillis())
-                .setNumber(1)
-                .setContentTitle("전송 알림")
-                .setContentText("유저 xx가 xx 파일을 전송하려 합니다.")
-                .setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE)
-                .setContentIntent(pendnoti)
-                .setAutoCancel(true)
-                .setOngoing(true);
-
-        //NotificationManager를 이용하여 푸시 알림 보내기
-        notificationManager.notify(1, builder.build());
-    }
 }
 
